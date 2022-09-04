cbac0afb6dd748af91278b62399087fb1bbc03ef
Sude Nurfer <81217566+nurfer1@users.noreply.github.com>
2022-09-04 12:08:17 +0300

    Create get_next_line.h

---
+++ get_next_line.h
@@ -0,0 +1,22 @@
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stddef.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 31
+# endif
+
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strdup(const char *s1);
+size_t	ft_strlen(const char *s);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_strchr(const char *s, int c);
+char	*getstr(int fd, char *str);
+char	*getnewline(char *str);
+char	*getremain(char *str);
+char	*get_next_line(int fd);
+
+#endif
